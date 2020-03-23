79fc4d117b7174885240ede34992f24505fe1437
vplacido <valentinplacido25@gmail.com>
2020-03-23 16:16:02 -0400

    added ability to create new user who is not in api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,8 @@ class UsersController < ApplicationController
         user = User.find(params[:id])
         render json: user
     end
+    def create 
+        user = User.find_or_create_by(username: params[:username])
+        render json: user
+    end
 end
